75bbd110cf44b5f69edbb8172ad973f9c4e6905c
Yulibeth Rivero <yulibeth.rivero@gmail.com>
2025-05-17 09:47:26 +0200

    Determinar fin de scroll

--- src/app/gifs/pages/trending-page/trending-page.component.ts
+++ src/app/gifs/pages/trending-page/trending-page.component.ts
@@ -19,10 +19,22 @@ export default class TrendingPageComponent {
   // gifs = signal(imageUrls);
   // gifs = computed(() => this.gifsService.trendingGifs());
 
-  scrollDivRef = viewChild<ElementRef>('groupDiv');
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
 
   onScroll(event: Event) {
     // ponemos ? porque en el momento que el componente se construye todavía no existe el html
     const scrollDiv = this.scrollDivRef()?.nativeElement;
+    if (!scrollDiv) return;
+
+    const scrollTop = scrollDiv.scrollTop;
+    const clienHeight = scrollDiv.clientHeight; // altura del viewport
+    const scrollHeight = scrollDiv.scrollHeight; // altura total del div
+
+    console.log({ scrollTop, clienHeight, scrollHeight });
+    console.log({ scrollTotal: scrollTop + clienHeight, scrollHeight });
+
+    const isAtBottom = scrollTop + clienHeight + 300 >= scrollHeight;
+
+    console.log({ isAtBottom });
   }
 }
